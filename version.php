1fc58d5ba8045e3fa8aa0ad899263ffc900e4f9b
valery <valery.fremaux@gmail.com>
2016-08-12 12:07:27 +0200

    fix version

--- version.php
+++ version.php
@@ -27,7 +27,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2016051500;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2015111000;        // Requires this Moodle version
+$plugin->requires  = 2016051900;        // Requires this Moodle version
 $plugin->component = 'block_shop_access'; // Full name of the plugin (used for diagnostics)
-$plugin->release = "3.0.0 (Build 2016022500)";
+$plugin->release = "3.1.0 (Build 2016022500)";
 $plugin->dependencies = array('local_shop' => 2016022500, 'auth_ticket' => '2012060400');
