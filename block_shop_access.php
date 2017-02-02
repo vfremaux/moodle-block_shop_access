ca7ecbbdd8a1fdedecb22b1d921e4794c35a5ebd
valery <valery.fremaux@gmail.com>
2017-02-02 18:58:42 +0100

    add verison 3.2

--- block_shop_access.php
+++ block_shop_access.php
@@ -32,7 +32,7 @@ class block_shop_access extends block_base {
     }
 
     public function has_config() {
-        return true;
+        return false;
     }
 
     public function specialization() {
